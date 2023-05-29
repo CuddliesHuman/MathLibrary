fe3492de68a95178f4d8124b39bc79442013854c
Denis Korniienko <kubikrubikmegaminks@gmail.com>
2023-05-29 17:50:08 +0300

    updated: Into the file .gitignore was added .vs for it's ignoring

--- MathLibrary/MathLibrary.h
+++ MathLibrary/MathLibrary.h
@@ -1,2 +1,25 @@
 #pragma once
 
+template<typename T>
+T Sum(T firstNumber, T secondNumber)
+{
+	return { firstNumber + secondNumber };
+}
+
+template<typename T>
+T Divide(T firstNumber, T secondNumber)
+{
+	return { firstNumber / secondNumber };
+}
+
+template<typename T>
+T Subtract(T firstNumber, T secondNumber)
+{
+	return { firstNumber - secondNumber };
+}
+
+template<typename T>
+T Multiply(T firstNumber, T secondNumber)
+{
+	return { firstNumber * secondNumber };
+}
